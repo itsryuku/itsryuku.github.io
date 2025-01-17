e4cb9f76382e4b9e3acd238ccdc7c49976e15a36
itsryuku <39871872+itsryuku@users.noreply.github.com>
2025-01-16 22:41:09 -0500

    ...

--- aaa.js
+++ aaa.js
@@ -1,3 +1,8 @@
-fetch('https://www.bwin.de/de/mobileportal/api/PersonalDetails', { credentials: 'include' })
+// fetch('https://www.bwin.de/de/mobileportal/api/PersonalDetails', { credentials: 'include' }).then(res => res.json()).then(console.log);
+//
+fetch('https://www.bwin.de/de/api/clientconfig', {
+  credentials: 'include',
+  headers: { 'X-Bwin-Browser-Referrer': 'https://sports.bwin.de/' }
+})
   .then(res => res.json())
   .then(console.log);
